5797707bce88c87c8c88ead3cd557f17f4ac05ff
sudolivictoria <ovquintanilla@istu.gob.sv>
2026-03-27 14:59:14 -0600

    Fix: Ordering

--- index.php
+++ index.php
@@ -168,6 +168,7 @@
     <script>
         $(document).ready(function() {
             var table = $('#tablaExonerados').DataTable({
+                "ordering": false,
                 language: {
                     url: '//cdn.datatables.net/plug-ins/1.13.6/i18n/es-ES.json'
                 },
